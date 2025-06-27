3483163b3d8ddd917ba23849660be7fe5209ceb5
himalsubedi <himal.kritim@gmail.com>
2025-06-27 11:15:00 +0545

    new

--- back-end/src/main/java/com/example/back_end/Config/UserConfig.java
+++ back-end/src/main/java/com/example/back_end/Config/UserConfig.java
@@ -1,5 +1,18 @@
 package com.example.back_end.Config;
 
-public class UserConfig {
-    ok done bro
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class UserConfig implements WebMvcConfigurer {
+
+    private String uploadDir;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("file:" + uploadDir);
+    }
 }
